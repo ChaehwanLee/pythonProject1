963a66344a92b07672c98ed0daeec945e74254b4
Chaehwan Lee <oasisinyourmind@gmail.com>
2022-02-15 14:18:05 +0900

    2.15 Class 변경 문제가 있음

--- ch153/account.py
+++ ch153/account.py
@@ -8,16 +8,21 @@
 # 1. 속성 : accno, balance, owner
 # 2. 행위 : deposit, widthdraw, inquire
 # UML(Unified Modeling Language) / db, modeling 을 잘하는 사람 but 둘 다 잘해야한다.
+from ch153.bank import interest
+
 
 class Account():
     # Constructor 생성, 역할을 초기화 시켜주는 것
     def __init__(self, accno, balance, owner):
         self.__accno = accno;
-        self.__balance = balance;
+        if balance <= 0:
+            self.__balance = 0;
+        else:
+            self.__balance = balance;
         self.__owner = owner;
 
-    def setowner(self, owner):  # getter 와 setter
-        self.__owner = owner;
+    # def setowner(self, owner):  # getter 와 setter
+    #     self.__owner = owner;
 
     @property
     def owner(self):
@@ -53,28 +58,36 @@ class Account():
 
 # 3. Inheritance (상속)
 
+# class IAccount(Account):
+#     def __init__(self, accno, balance, name, interest):  # 속성정보세팅
+#         super().__init__(accno, balance, name);
+#         self.__interest = interest;
+#
+#     def __str__(self):  # 이자까지 표현
+#         return super().__str__() + ' ' + str(self.__interest);  # float값이다
+#
+#     def calcinterest(self):  # 이자값 연산
+#         return super().inquire() * (self.__interest / 100);
+#
+#     def getinterest(self):
+#         return self.__interest;
+#
+#     def setinterest(self, interest):
+#         self.__interest = interest;
+
 class IAccount(Account):
-    def __init__(self, accno, balance, name, interest):  # 속성정보세팅
+    def __init__(self, accno, balance, name, interest):
         super().__init__(accno, balance, name);
         self.__interest = interest;
 
-    def __str__(self):  # 이자까지 표현
-        return super().__str__() + ' ' + str(self.__interest);  # float값이다
+    def __str__(self):
+        return super().__str__() + ' ' + str(self.__interest);
 
-    def calcinterest(self):  # 이자값 연산
+    def calcinterest(self):
         return super().inquire() * (self.__interest / 100);
 
     def getinterest(self):
         return self.__interest;
 
-    def setinterest(self, interest):
-        self.__interest = interest;
-
-# class IAccount(Account):
-#     def __init__(self,accno, balance, name, interest):
-#         super().__init__(accno,balance,name);
-#         self.__interest = interest;
-#     def __str__(self):
-#         return super().__str__()+' '+str(self.__interest);
-#     def calcinterest(self):
-#         return super().inquire() * (self.__interest / 100);
+    def setinterest(self):
+        return self.__interest = interest;
